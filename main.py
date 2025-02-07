de0b20b331101ed960df8222713769fb409fb7af
hanyangzhoupiano <130773491+hanyangzhoupiano@users.noreply.github.com>
2025-02-07 18:16:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,9 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 @bot.event
 async def on_ready():
   print("ready!")
-  channel = bot.get_channel(1321480987238203454)
-  await channel.send("Hello, testing testing testing this new bot I coded which will be the leveling bot")
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send("Pong! I am a super cool bot!")
+async def hello(ctx):
+    await ctx.send("Hello! I am a bot programmed by Hanyang Zhou.")
 
 bot.run("MTI5NjYwMTU0MDM4NzE0Nzg2OQ.GpBnjT.x7weeYWyRHXfCG6RGXsYsbGoZBNZq_WDRHAPWc")

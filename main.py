4f8beea8da60b8c145c6a1dfb84c1491c7b8400f
hanyangzhoupiano <130773491+hanyangzhoupiano@users.noreply.github.com>
2025-02-07 18:01:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import discord
 from discord.ext import commands
 
+intents = discord.Intents.default()
+intents.messages = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
 @bot.event
 async def on_ready():
   print("ready!")
   channel = bot.get_channel(1321480987238203454)
   await channel.send("Hello, testing testing testing this new bot I coded which will be the leveling bot")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong! I am a super cool bot!")

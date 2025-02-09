52b073e84080717e76be90d15adc5bf017644fdd
hanyangzhoupiano <130773491+hanyangzhoupiano@users.noreply.github.com>
2025-02-09 11:34:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,18 @@
 import os
 import discord
 import json
+import random
 from discord.ext import commands
-from flask import Flask
 from threading import Thread
 
-# Create a simple web server
-app = Flask(__name__)
-@app.route('/')
-def home():
-    return "Bot is running!"
-def run_web():
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
-Thread(target=run_web).start()
+import data_functions
 
 intents = discord.Intents.all()
 intents.messages = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-def load_data():
-    try:
-        with open("data.json", "w") as file:
-            json.load(file)
-    except FileNotFoundError:
-        return {}
+data = data_functions.load_data()
 
 @bot.event
 async def on_ready():
@@ -35,20 +22,25 @@ async def on_ready():
 async def on_message(msg):
     if msg.author == bot.user:
         return
-    message = msg.content
-    if "ok" in message.lower() or "okay" in message.lower():
-        await msg.channel.send(f"Okay, too!")
-    if "hi" in message.lower() or "hello" in message.lower():
-        await msg.channel.send(f"Hello! I am a bot and would like to engage in this conversation.")
-    if "yay" in message.lower():
-        await msg.channel.send(f"Yay to you! What are you so happy about?")
-    if "oh no" in message.lower():
-        await msg.channel.send(f"Oh no! What has gone wrong?")
-    if "work" in message.lower():
-        await msg.channel.send(f"Wow, you really like working, " + msg.author.name + "!")
+    user = msg.author.id
+    
 
 @bot.command()
 async def hello(ctx):
     await ctx.send("Hello! I am a bot programmed by Hanyang Zhou.")
 
 bot.run("MTI5NjYwMTU0MDM4NzE0Nzg2OQ.GpBnjT.x7weeYWyRHXfCG6RGXsYsbGoZBNZq_WDRHAPWc")
+
+from flask import Flask
+
+# Create a simple web server
+app = Flask(__name__)
+@app.route('/')
+
+def home():
+    return "Bot is running!"
+
+def run_web():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+Thread(target=run_web).start()

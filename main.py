76ec604493d5f321e643eddd0dcb6cb565dd5ea5
hanyangzhoupiano <130773491+hanyangzhoupiano@users.noreply.github.com>
2025-02-10 22:22:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ intents.messages = True
 default_prefixes = {"!"}
 prefixes = {}
 user_last_experience_time = {}
+experience_drops = {}
 
 bot = commands.Bot(command_prefix=lambda bot, message: data_functions.get_prefix(message.guild.id), intents=intents)
 
@@ -92,7 +93,7 @@ async def help(ctx, command_name: str = None):
 
 @bot.command(aliases=["vp", "viewp"], help="Shows the current prefix of this bot.")
 @commands.cooldown(2, 10, commands.BucketType.user)
-async def viewprefix(ctx):
+async def view_prefix(ctx):
     await ctx.send(embed=discord.Embed(
         color=int("50B4E6", 16),
         description=f"The current prefix is '{data_functions.get_prefix(ctx.guild.id)}'.",
@@ -100,7 +101,7 @@ async def viewprefix(ctx):
 
 @bot.command(aliases=["sp", "newp"], help="Changes the prefix of this bot.")
 @commands.cooldown(2, 10, commands.BucketType.user)
-async def setprefix(ctx, new_prefix: str = None):
+async def set_prefix(ctx, new_prefix: str = None):
     if ctx.author.guild_permissions.manage_guild and new_prefix is not None:
         if len(new_prefix) > 32:
             await ctx.send(embed=discord.Embed(
@@ -116,7 +117,7 @@ async def setprefix(ctx, new_prefix: str = None):
         
 @bot.command(aliases=["vs", "msgs", "lvls", "stats"], help="Shows the statistics of a user, such as levels, experience, and messages.")
 @commands.cooldown(2, 10, commands.BucketType.user)
-async def viewstats(ctx, name: str = None):
+async def view_stats(ctx, name: str = None):
     if not ctx.author.bot:
         user = None
         if name is None:
@@ -198,45 +199,97 @@ async def viewstats(ctx, name: str = None):
                 description=f"**Level:** {level}\n**Experience:** {experience}\n**Until Next Level:** {experience_left}\n**Messages:** {messages}\n**Server Join Date:** {user.joined_at.strftime('%m/%d/%Y').lstrip('0').replace('/0', '/')}\n**Role:** {user.top_role}",
             ).set_author(name=user.name, icon_url=user.avatar.url))
 
-@bot.command(aliases=["ec"], help="Repeats the inputted message.")
-@commands.cooldown(2, 5, commands.BucketType.user)
-async def echo(ctx, *, message: str = None):
+@bot.command(aliases=["expdrop, edrop, expd, ed"], help="Create an experience drop of a specified amount.")
+@commands.cooldown(1, 10, commands.BucketType.user)
+async def experience_drop(ctx, amount: int = None):
+    if amount is not None amount.isnumberic():
+        global experience_drops
+        if amount <= 1000:
+            drop_index = random.random * 1000 + 1
+            experience_drops[drop_index] = amount
+            await ctx.send(embed=discord.Embed(
+                color=int("50B4E6", 16),
+                title="Experience Drop",
+                description=f"An experience drop of {amount} has started! Type 'claim' to claim it before the time runs out!",
+            ))
+            try:
+                response = await bot.wait_for('message', check=lambda msg: msg.channel == ctx.channel, timeout=10.0)
+                if response.content.lower() == "claim":
+                    if not ctx.author.bot:
+                        if drop_index in experience_drops:
+                            del experience_drops[drop_index]
+                        await ctx.send(embed=discord.Embed(
+                            color=int("50B4E6", 16),
+                            title="Experience Drop",
+                            description=f"*{ctx.author.name}* was the first to claim the experience drop of {amount}!",
+                        ))
+                        data_functions.set_experience(ctx.author.id, data_functions.get_experience(ctx.author.id) + amount)
+                        
+            except asyncio.TimeoutError:
+                await ctx.send(embed=discord.Embed(
+                    color=int("FA3939", 16),
+                    description="Nobody claimed the experience drop in time.",
+                ).set_author(name=ctx.author.name, icon_url=ctx.author.avatar.url))
+                return
+        else:
+            await ctx.send(embed=discord.Embed(
+                color=int("FA3939", 16),
+                description="You can only drop 1000 experience at a time.",
+            ).set_author(name=ctx.author.name, icon_url=ctx.author.avatar.url))
+
+@bot.command(aliases=["s"], help="Make the bot say a specified message.")
+@commands.cooldown(2, 10, commands.BucketType.user)
+async def say(ctx, *, message: str = None):
     if message is not None:
         await ctx.send(embed=discord.Embed(
             color=int("50B4E6", 16),
             description=message,
         ).set_author(name=ctx.author.name, icon_url=ctx.author.avatar.url))
+
 @bot.command(aliases=["addl", "al"], help="Adds levels to the specificed user.")
-@commands.cooldown(1, 5, commands.BucketType.user)
-async def addlevels(ctx, amount: int = None, password: str = None):
-    if amount is not None and password is not None:
-        if password.lower() == "doom":
-            if int(amount):
+@commands.cooldown(2, 10, commands.BucketType.user)
+async def add_levels(ctx, amount: int = None):
+    if amount is not None and ctx.author.guild_permissions.manage_guild:
+        if amount.isnumeric():
+            if amount <= 10:
                 data_functions.set_levels(ctx.author.id, int(amount))
-            await ctx.send(embed=discord.Embed(
-                color=int("50B4E6", 16),
-                description=f"Successfully {added_levels} levels to {ctx.author.name}.",
-            ).set_author(name=ctx.author.name, icon_url=ctx.author.avatar.url))
+                await ctx.send(embed=discord.Embed(
+                    color=int("50B4E6", 16),
+                    description=f"Successfully {amount} levels to {ctx.author.name}.",
+                ).set_author(name=ctx.author.name, icon_url=ctx.author.avatar.url))
+            else:
+                await ctx.send(embed=discord.Embed(
+                    color=int("FA3939", 16),
+                    description="You can only add a maximum of 10 levels at a time.",
+                ).set_author(name=ctx.author.name, icon_url=ctx.author.avatar.url))
+                return
         else:
             await ctx.send(embed=discord.Embed(
-                color=int("50B4E6", 16),
-                description=f"Incorrent password: {password}",
+                color=int("FA3939", 16),
+                description="Invalid amount.",
             ).set_author(name=ctx.author.name, icon_url=ctx.author.avatar.url))
+
 @bot.command(aliases=["addm", "am"], help="Adds messages to the specified user.")
 @commands.cooldown(1, 5, commands.BucketType.user)
-async def addmessages(ctx, amount: int = None, password: str = None):
-    if amount is not None and password is not None:
-        if password.lower() == "doom":
-            if int(amount):
+async def add_messages(ctx, amount: int = None):
+    if amount is not None and ctx.author.guild_permissions.manage_guild:
+        if amount.isnumeric():
+            if amount <= 10:
                 data_functions.set_messages(ctx.author.id, int(amount))
-            await ctx.send(embed=discord.Embed(
-                color=int("50B4E6", 16),
-                description=f"Successfully {added_messages} messages to {ctx.author.name}.",
-            ).set_author(name=ctx.author.name, icon_url=ctx.author.avatar.url))
+                await ctx.send(embed=discord.Embed(
+                    color=int("50B4E6", 16),
+                    description=f"Successfully {amount} messages to {ctx.author.name}.",
+                ).set_author(name=ctx.author.name, icon_url=ctx.author.avatar.url))
+            else:
+                await ctx.send(embed=discord.Embed(
+                    color=int("FA3939", 16),
+                    description="You can only add a maximum of 10 messages at a time.",
+                ).set_author(name=ctx.author.name, icon_url=ctx.author.avatar.url))
+                return
         else:
             await ctx.send(embed=discord.Embed(
-                color=int("50B4E6", 16),
-                description=f"Incorrent password: {password}",
+                color=int("FA3939", 16),
+                description="Invalid amount.",
             ).set_author(name=ctx.author.name, icon_url=ctx.author.avatar.url))
 
 bot.run(os.getenv("DISCORD_TOKEN"))

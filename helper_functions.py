c0e0cd7ef5b527b26fc3eebcf8dd1c6d6bf9a74f
hanyangzhoupiano <130773491+hanyangzhoupiano@users.noreply.github.com>
2025-02-11 16:51:06 -0500

    Create helper_functions.py

---
+++ helper_functions.py
@@ -0,0 +1,34 @@
+import asyncio
+import discord
+
+def experience_drop(ctx, amount):
+    if amount is not None and str(amount).isnumeric():
+        global experience_drops
+        if amount <= 10000:
+            await ctx.send(embed=discord.Embed(
+                color=int("50B4E6", 16),
+                title="Experience Drop",
+                description=f"An experience drop of {amount} has started! Type 'claim' to claim it before the time runs out!",
+            ))
+            try:
+                response = await bot.wait_for('message', check=lambda msg: msg.channel == ctx.channel, timeout=10.0)
+                if response.content.lower() == "claim":
+                    if not response.author.bot:
+                        await ctx.send(embed=discord.Embed(
+                            color=int("50B4E6", 16),
+                            title="Experience Drop",
+                            description=f"*{response.author.name}* was the first to claim the experience drop of {amount}!",
+                        ))
+                        data_functions.set_experience(response.author.id, data_functions.get_experience(ctx.author.id) + amount)
+                        
+            except asyncio.TimeoutError:
+                await ctx.send(embed=discord.Embed(
+                    color=int("FA3939", 16),
+                    description="Nobody claimed the experience drop in time.",
+                ).set_author(name=ctx.author.name, icon_url=ctx.author.avatar.url))
+                return
+        else:
+            await ctx.send(embed=discord.Embed(
+                color=int("FA3939", 16),
+                description="You can only drop 10,000 experience at a time.",
+            ).set_author(name=ctx.author.name, icon_url=ctx.author.avatar.url))
